6ec69cabb25c477047061d8549ceadea7caae5a3
Rowan Crawford <wombleton@gmail.com>
2011-03-22 18:50:50 +1300

    Add in showing and listing moves

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-var app = require('express').createServer();
+var app = require('express').createServer(),
+    _ = require('underscore');
 
+app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
 app.get('/', function(req, res) {
@@ -7,5 +9,23 @@ app.get('/', function(req, res) {
 });
 
 var moves = require('./moves')
+
+app.get('/moves', function(req, res) {
+  res.render('moves/index', { locals: {
+    moves: moves.all(),
+    _: _
+  }});
+});
+
+app.get('/moves/:id', function(req, res) {
+  var id = req.params.id,
+      move = moves.find(id);
+  res.render('moves/show', {
+    locals: {
+      move: move,
+      _: _
+    }
+  });
+});
 
 app.listen(3000);
--- moves.js
+++ moves.js
@@ -1,32 +1,34 @@
-var moves = [
-  {
-    condition: 'you *do something under fire*, or dig in to endure fire',
-    slug: 'do-something-under-fire',
-    roll: '+cool',
-    success: 'you do it',
-    partial: 'you flinch, hesitate, or stall: the MC can offer you a worse outcome, a hard bargain, or an ugly choice.',
-    failure: 'the MC can make as hard a move as they like',
-    created_at: new Date()
-  },
-  {
-    condition: 'you *go aggro on someone*',
-    slug: 'go-aggro',
-    roll: '+hard',
-    success: 'they have to choose: force your hand and suck it up, or cave and do what you want',
-    partial: 'they can instead choose 1:n* get the hell out of your way\n* barricade themselves securely in\ngive you something they think you want\n*back off calmly, hands where you can see\n*tell you what you want to know (or what you want to hear)',
-    failure: 'the MC can make as hard a move as they like',
-    created_at: new Date()
-  }
-];
-
-module.export.all = moves
-
-module.export.find = function(slug) {
-  var move;
-  for (move in moves) {
-    if (move.slug === slug) {
-      return move;
+(function() {
+  var moves,
+      _ = require('underscore');
+  moves = [
+    {
+      condition: 'you *do something under fire*, or dig in to endure fire',
+      slug: 'do-something-under-fire',
+      roll: '+cool',
+      success: 'you do it',
+      partial: 'you flinch, hesitate, or stall: the MC can offer you a worse outcome, a hard bargain, or an ugly choice.',
+      failure: 'the MC can make as hard a move as they like',
+      created_at: new Date()
+    },
+    {
+      condition: 'you *go aggro on someone*',
+      slug: 'go-aggro',
+      roll: '+hard',
+      success: 'they have to choose: force your hand and suck it up, or cave and do what you want',
+      partial: 'they can instead choose 1:\n* get the hell out of your way\n* barricade themselves securely in\ngive you something they think you want\n*back off calmly, hands where you can see\n*tell you what you want to know (or what you want to hear)',
+      failure: 'the MC can make as hard a move as they like',
+      created_at: new Date()
     }
+  ];
+
+  module.exports.all = function() {
+    return moves;
   }
-  return null;
-}
+
+  module.exports.find = function(slug) {
+    return _.detect(moves, function(move) {
+      return move.slug === slug;
+    });
+  }
+}());
