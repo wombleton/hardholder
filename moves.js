e88d24bd69a74fb2d59482a37080c063c3ac6be8
Rowan Crawford <wombleton@gmail.com>
2011-03-28 22:08:24 +1300

    updating to latest

--- moves.js
+++ moves.js
@@ -3,17 +3,67 @@
       _ = require('underscore'),
       Mongoose = require('mongoose'),
       Schema = Mongoose.Schema,
-      Move;
+      MoveSchema;
 
-  Move = new Schema({
+  function slug(s) {
+    return (s || '').replace(/[^a-zA-Z0-9]/, '-').replace(/[-]+/, '-');
+  }
+
+  MoveSchema = new Schema({
     condition: String,
     slug: String,
     roll: String,
+    preamble: String,
     success: String,
     partial: String,
     failure: String,
+    postamble: String,
     created_at: Date
-  });
+  }) ;
 
-  Mongoose.model('Move', Move);
+  Mongoose.model('Move', MoveSchema);
+
+  module.exports.route = function(app, Move) {
+    app.get('/moves', function(req, res) {
+      Move.find({}, function(err, docs) {
+        res.render('moves/index', { locals: {
+          moves: docs
+        }});
+      });
+    });
+
+    app.post('/moves', function(req, res) {
+      var move = new Move(req.body.move);
+
+      move.save(function() {
+        res.redirect('/moves/' + move._id.toHexString());
+      });
+    });
+
+    app.get('/moves/new', function(req, res) {
+      res.render('moves/new');
+    });
+
+    app.get('/moves/:id', function(req, res) {
+      Move.findOne({ slug: req.params.id }, function(err, move) {
+        res.render('moves/show', {
+          locals: {
+            move: move,
+            _: _
+          }
+        });
+      });
+    });
+
+    app.get('/moves/:id/edit', function(req, res) {
+      var id = req.params.id,
+          move = Moves.find(id);
+      res.render('moves/edit', {
+        locals: {
+          move: move,
+          _: _
+        }
+      });
+    });
+  };
 }());
--- server.js
+++ server.js
@@ -1,49 +1,24 @@
-var express = require('express').createServer(),
+var express = require('express'),
+    app = express.createServer(),
     _ = require('underscore'),
     Mongoose = require('mongoose'),
     db = Mongoose.connect('mongodb://localhost/db');
 
-express.set('views', __dirname + '/views');
-express.set('view engine', 'jade');
+app.configure(function() {
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.static(__dirname + '/static'))
+});
 
-express.get('/', function(req, res) {
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res) {
   res.render('index');
 });
 
 var moves = require('./moves'),
     Moves = db.model('Move');
+moves.route(app, Moves);
 
-express.get('/moves', function(req, res) {
-  Moves.find({}, function(err, docs) {
-    res.render('moves/index', { locals: {
-      moves: docs,
-      _: _
-    }});
-  });
-});
-
-express.get('/moves/:id', function(req, res) {
-  Moves.findOne({ slug: req.params.id }, function(err, move) {
-    res.render('moves/show', {
-      locals: {
-        move: move,
-        _: _
-      }
-    });
-  });
-  var id = req.params.id,
-      move = Moves.find({ slug: id });
-});
-
-express.get('/moves/:id/edit', function(req, res) {
-  var id = req.params.id,
-      move = Moves.find(id);
-  res.render('moves/edit', {
-    locals: {
-      move: move,
-      _: _
-    }
-  });
-});
-
-express.listen(3000);
+app.listen(3000);
